364db5f9d05006762dca10b339c6daa0ae20e573
ghost <kirill.han011@gmail.com>
2022-05-01 22:58:56 +0600

    Реализован вывод всех записей на главную страницу

--- book/views.py
+++ book/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from book.models import GuestBook
+
+
+def index_view(request):
+    guest_books = GuestBook.objects.filter(status="active").order_by("-time_of_creation")
+    return render(request, 'index.html', {'guest_books': guest_books})
