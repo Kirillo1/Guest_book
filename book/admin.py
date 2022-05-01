dd2d968f9d6b3133d92c656ee92195d4d2bc2f19
ghost <kirill.han011@gmail.com>
2022-05-01 22:53:51 +0600

    Добавлена модель GuestBook и создана админ панель. Созданы и применены миграции. Добавлен супер юзер

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from book.models import GuestBook
+
+
+class GuestBookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'status', 'post_text']
+    list_filter = ['time_of_creation']
+    search_fields = ['name', 'status']
+    fields = ['name', 'status', 'post_text', 'mail', 'time_of_creation', 'update_time']
+    readonly_fields = ['time_of_creation', 'update_time']
+
+
+admin.site.register(GuestBook, GuestBookAdmin)
--- book/models.py
+++ book/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [('active', 'active'), ('blocked', 'blocked')]
+
+
+class GuestBook(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False, verbose_name="Name")
+    mail = models.EmailField(max_length=40, null=False, blank=False, verbose_name="Email")
+    post_text = models.TextField(max_length=2000, null=False, blank=False, verbose_name="Your text")
+    time_of_creation = models.DateTimeField(auto_now_add=True, verbose_name="Time of creation")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="Update time")
+    status = models.CharField(max_length=30, null=False, blank=False, verbose_name="status", default="active",
+                              choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f"{self.pk}. {self.name}: {self.status}"
+
+    class Meta:
+        db_table = 'guest_book'
+        verbose_name = 'book'
+        verbose_name_plural = 'books'
